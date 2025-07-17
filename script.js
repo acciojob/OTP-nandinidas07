0407791d589bd9d2463751ab5c1505d35a0969df
nandinidas07 <nandinidas107@gmail.com>
2025-07-17 22:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
   input.addEventListener("input", (e) => {
     const value = e.target.value;
 
-    // Only allow digits
+    // Only allow 0-9
     if (!/^[0-9]$/.test(value)) {
       e.target.value = "";
       return;
     }
 
-    // Move to next input if any
+    // Move to next
     if (index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
@@ -20,24 +19,26 @@ inputs.forEach((input, index) => {
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
       if (input.value === "") {
-        // Move to previous field if current is empty
+        // If current is empty, go to previous
         if (index > 0) {
           inputs[index - 1].focus();
         }
       } else {
-        // Clear current field if not empty
+        // Clear current value
         input.value = "";
       }
     }
   });
 
-  // Allow arrow keys to move between inputs
-  input.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowLeft" && index > 0) {
-      inputs[index - 1].focus();
-    }
-    if (e.key === "ArrowRight" && index < inputs.length - 1) {
-      inputs[index + 1].focus();
-    }
+  input.addEventListener("paste", (e) => {
+    e.preventDefault();
+    const paste = (e.clipboardData || window.clipboardData).getData("text").slice(0, 6);
+    paste.split('').forEach((char, i) => {
+      if (i < inputs.length) {
+        inputs[i].value = char;
+      }
+    });
+    const next = paste.length < inputs.length ? paste.length : inputs.length - 1;
+    inputs[next].focus();
   });
 });
